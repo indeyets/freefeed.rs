6104252b47210cf4b0cbd8cd8b3c128daef87027
Alexey Zakhlestin <indeyets@gmail.com>
2020-05-03 13:57:09 +0300

    Introduce CLI interface: "frf"
    - `frf me` returns current username

---
+++ src/bin/frf/main.rs
@@ -0,0 +1,64 @@
+use std::env;
+use std::process::exit;
+use std::ops::Deref;
+
+use clap::Clap;
+use freefeed::api::api_client;
+
+#[derive(Clap)]
+struct Opts {
+    /// Specify API origin. Will fall back to FRF_ORIGIN or "https://candy.freefeed.net/"
+    #[clap(short, long)]
+    origin: Option<String>,
+    /// Specify your API Token. Will fall-back to FRF_TOKEN
+    #[clap(short, long)]
+    token: Option<String>,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Clap)]
+enum Command {
+    Me(MeOpts)
+}
+
+#[derive(Clap)]
+struct MeOpts {
+}
+
+#[tokio::main]
+async fn main() {
+    let opts: Opts = Opts::parse();
+
+    let origin = match opts.origin {
+        Some(val) => val,
+        None => match env::var("FRF_ORIGIN") {
+            Ok(val) => val,
+            Err(_e) => String::from("https://candy.freefeed.net"),
+        }
+    };
+
+    let token  = match opts.token {
+        Some(val) => val,
+        None => match env::var("FRF_TOKEN") {
+            Ok(val) => val,
+            Err(_e) => {
+                eprintln!("FRF_TOKEN env variable is not found\nConsider using https://direnv.net/ to set it");
+                exit(1);
+            }
+        }
+    };
+
+    match opts.command {
+        Command::Me(_) => {
+            let client = api_client(origin.deref(), Some(token.deref()));
+            match client.get_me().await {
+                Ok(val) => println!("{}", val),
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                    exit(1);
+                }
+            }
+        }
+    }
+}
