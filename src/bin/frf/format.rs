c815be2155d03eeef04f856e24d0bed9477a08e6
Alexey Zakhlestin <indeyets@gmail.com>
2020-05-10 18:59:46 +0300

    Implement terminal-aware word-wrapping for output

--- src/bin/frf/format.rs
+++ src/bin/frf/format.rs
@@ -1,8 +1,17 @@
+use textwrap::{Wrapper, termwidth};
 use freefeed::api::Post;
 
 pub fn format_post(val: Post) {
-    println!("From: {} ({}):\n", val.author.screen_name, val.author.username);
-    println!("{}", val.body);
+    let w = Wrapper::with_termwidth()
+        .initial_indent("")
+        .subsequent_indent("     ");
+    let from = format!("From: {} ({}):", val.author.screen_name, val.author.username);
+    println!("{}\n", w.fill(&from));
+
+    let w = Wrapper::new(termwidth() - 2)
+        .initial_indent("  ")
+        .subsequent_indent("  ");
+    println!("{}", w.fill(&val.body));
 
     if val.attachments.len() > 0 {
         println!("\nAttachments:");
